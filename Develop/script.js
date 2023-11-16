f23c808d6007ee63ad64d3c931f72241f2e38d6f
paulina koch <pjassakoch@gmail.com>
2023-11-16 00:02:51 -0500

    Added prompt for character type

--- Develop/script.js
+++ Develop/script.js
@@ -19,7 +19,7 @@ generateBtn.addEventListener("click", writePassword);
 
 //promt for choosing a number of the characters
 
-// var LengthChoice = prompt("Choose a length between 8 and 128.");
+ var LengthChoice = prompt("Choose a length between 8 and 128.");
 
 //choice of password length
 /*
@@ -47,16 +47,26 @@ if (LengthChoice < 8 || LengthChoice > 128); {
   
 }
 */
-//just tesing what this does to the website?
-function generatePassword() {
-  console.log("Will I figure this out??")
+
+//function generatePassword(LengthChoice) {
+  if (LengthChoice < 8 || LengthChoice > 128); {
+    alert("You must choose a length between 8 and 128!");
+    window.open("index.htm");
+  } 
+  else {
+
+    return LengthChoice;
 }
+
+//3. to do list character type prompt
+var characterType = window.prompt("Select at least one type of characters (lowercase, uppercase, numeric, and/or special characters");
 
 /*to do list for generating function:
 1. Prompt 1- lenght
-3. Store length input
-2. Prompt 2- character types
+2. Store length input
+3. Prompt 2- character types
 3. Create array for character types?
-4. store character type input
-5. one character type selected
-6. return password
+5. store character type input
+6. one character type selected for password
+7. return password
+*/
