57b4acc18b5bf0eb2be9c05fe9714866972de5ef
paulina koch <pjassakoch@gmail.com>
2023-11-16 21:38:18 -0500

    Created a alert window to prompt that at least one character type must be selected

--- Develop/script.js
+++ Develop/script.js
@@ -23,6 +23,8 @@ generateBtn.addEventListener("click", writePassword);
   b. uppercase
   c. numeric
   d. special characters
+5. Validate if one character type was selected.
+ b. create object for storing user confirmations
 4. Create array OR VAR for character types?
 5. store character type input IN VAF
 6. one character type selected for password
@@ -48,12 +50,15 @@ function generatePassword() {
   //1. Prompt 1- lenght
   var lengthChoice = prompt("Choose a length between 8 and 128.");
 
-  // b.  confirming LengthChoice is between 8-128
+  // b.  confirming LengthChoice is between 8-128 but keeps going back if condition is not met.
   while (lengthChoice < 8 || lengthChoice > 128) {
     alert("You must choose a length between 8 and 128!");
     var lengthChoice = prompt("Choose a length between 8 and 128.");}
     console.log(lengthChoice);
-  
+ 
+ //alert for at least one characer choice
+ window.alert ("Please select at least ONE lowercase, or uppercase, or number or special character to include in your password");   
+
 // prompt for user lowercase choice
 
 var lowercaseConfirm = confirm("Do you want to inlcude lowercase in your password?");
@@ -67,3 +72,9 @@ var numericConfirm = confirm("Do you want to include numeric values in your pass
 //prompt for special characters choice
 var specialConfirm = confirm("Do you want to include special characters in your password?")
 }
+
+//Need to validate if at least one of the character choices was selected. 
+//First create an array to store the confirmations then 
+//create a for loop to check the array
+
+let validationArray = [ ]
